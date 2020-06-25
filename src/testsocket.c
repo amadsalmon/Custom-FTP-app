bbce3bf6387c399a353fe0172ba7d32205521e79
Amad Salmon <amad.salmon@etu.univ-cotedazur.fr>
2020-06-25 02:39:07 +0200

    test: added testsocket for UDP mode

---
+++ src/testsocket.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <curses.h> 		/* Primitives de gestion d'ecran */
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "fon.h"   		/* primitives de la boite a outils */
+
+int main(int argc, char const *argv[])
+{
+    /*  Définition du serveur  */
+    char serveur_IP[] = "127.0.0.1";
+    char serveur_port[] = "23580";
+    struct sockaddr_in *p_adr_socket_s;
+    adr_socket(serveur_port, serveur_IP, SOCK_DGRAM, &p_adr_socket_s);
+
+    /*  Définition du client  */
+    char client_IP[] = "127.0.0.1";
+    char client_port[] = "28000";
+    int client_socket = h_socket(AF_INET,SOCK_DGRAM);
+    struct sockaddr_in *p_adr_socket_c;
+    adr_socket(client_port, client_IP, SOCK_DGRAM, &p_adr_socket_c);
+    h_bind(client_socket, p_adr_socket_c);
+
+
+    /*  Définition du buffer  */
+    int taille_buffer = 8; // taille en octets
+    // 1 char ASCII = 1 octet donc "Bonjour\O"=8 octets
+    char buffer[] = "Bonjour";
+
+    /*  Envoi  */
+    h_sendto(client_socket, buffer, taille_buffer, p_adr_socket_s);
+
+    return 0;
+}
